5574a93c970ff1381e0de2564796d54fce7386b1
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-04-07 13:11:25 -0600

    added bootstrap and finished sign-up

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} Money")
-end
-
-puts "100 Posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe 'navigate' do
 	describe 'index' do
-		before do
-			visit posts_path
-		end
-		
 		it 'can be reached successfully' do
+			visit posts_path
 			expect(page.status_code).to eq(200)
 		end
 
 		it 'has a title of Posts' do
+			visit posts_path
 			expect(page).to have_content(/Posts/)
 		end
 	end
 
 	describe 'creation' do
 		before do
+			user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+			login_as(user, :scope => :user)
 			visit new_post_path
 		end
 
@@ -28,10 +28,17 @@ describe 'navigate' do
 
 			fill_in 'post[date]', with: Date.today
 			fill_in 'post[rationale]', with: "Some rationale"
-
 			click_on "Save"
 
 			expect(page).to have_content("Some rationale")
+		end
+
+		it 'will have a someuser associated with it' do
+			fill_in 'post[date]', with: Date.today
+			fill_in 'post[rationale]', with: "User Association"
+			click_on "Save"
+
+			expect(User.last.posts.last.rationale).to eq("User Association")
 		end
 	end
 end
