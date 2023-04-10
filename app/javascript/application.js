d7b4e51bb57f61f9bc21f3430dbec3c820dd390e
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-04-10 12:36:53 -0600

    logout and active helper for tabs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-//= require jquery
+// import 'jquery-sortablejs'
+//= require jquery3
+//= require popper
 //= require bootstrap-sprockets
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery
+// require jquery_ujs
+// require jquery-uiimport "@rails/request.js"
