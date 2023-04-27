bc8f5101db17c756d20abec5d359896474f5c06e
DocMAP <121587704+DocMAP@users.noreply.github.com>
2023-04-26 21:18:22 -0600

    added _past_due and fixed overflow formatting

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -8,4 +8,16 @@ class AuditLogsController < ApplicationController
         	redirect_to(root_path)
       	end
 	end
+
+	def confirm
+		audit_log = AuditLog.find(params[:id])
+
+		if audit_log.user_id = current_user.id || 'AdminUser'
+			audit_log.confirmed!
+			redirect_to root_path, notice: "The submission has been confirmed"
+		else
+			flash[:alert] = "You are not authorized to confirm this time entry."
+        	redirect_to(root_path)
+        end
+	end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,9 +2,9 @@ class StaticController < ApplicationController
 	def homepage
 		if admin_types.include?(current_user.type)
 			@pending_approvals = Post.where(status: 'submitted').page(params[:page]).per(5)
-			@pending_submissions = AuditLog.where(status: 'pending')
+			@pending_submissions = AuditLog.where(status: 'pending').page(params[:page]).per(10)
 		else
-			# something else
+			@pending_confirmations = current_user.audit_logs.where(status: 'pending')
 		end
 	end
 end
